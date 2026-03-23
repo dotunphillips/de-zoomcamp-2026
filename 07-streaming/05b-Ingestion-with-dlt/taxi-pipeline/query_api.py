3cfd1497d960258ae9ea020141b5842710e268e9
Dotun Phillips <47232916+dotunphillips@users.noreply.github.com>
2026-03-23 04:37:42 +0000

    fix: remove unwanted directories from 07-streaming

--- 07-streaming/05b-Ingestion-with-dlt/taxi-pipeline/query_api.py
+++
@@ -1,9 +0,0 @@
-import requests, json
-resp = requests.get('https://us-central1-dlthub-analytics.cloudfunctions.net/data_engineering_zoomcamp_api', params={'limit':1,'offset':0})
-print('status', resp.status_code)
-if resp.status_code==200:
-    with open('sample_record.json','w') as f:
-        json.dump(resp.json()[0], f, indent=2)
-    print('wrote sample_record.json')
-else:
-    print('request failed')
